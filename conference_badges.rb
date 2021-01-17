6a21d965c7a9b0420403a33a2439e4d0130897e4
rtyenriques <tooshopboys@gmail.com>
2021-01-16 23:28:08 -0800

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,23 +1,67 @@
-# Write your code here.
-def badge_maker(speaker)
-    return "Hello, my name is #{speaker}."
-   end
-   
-   speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-   
-   def batch_badge_creator(speakers_lineup)
-    speakers_lineup.collect {|speaker| badge_maker(speaker)}
-   end
-   
-   def assign_rooms(speakers_lineup)
-    speakers_lineup.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
-   end
-   
-   def printer(speakers)
-    batch_badge_creator(speakers).each do |badge|
-     puts badge
+
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(attendees)
+    attendees.collect do |name|
+      badge_maker(name)
     end
-    assign_rooms(speakers).each do |assignment|
-     puts assignment
+  
+  end
+  
+  def assign_rooms(attendees)
+    room=0
+    attendees.collect do |name|
+      room+=1
+      "Hello, #{name}! You'll be assigned to room #{room}!"
     end
-   end
+  end
+  
+  def printer(attendees)
+    batch_badge_creator(attendees).each do |value|
+      puts value
+    end
+    assign_rooms(attendees).each do |value|
+      puts value
+    end
+  end
+
+# def badge_maker(name)
+#  "Hello, my name is #{name}." 
+# end
+
+
+# def batch_badge_creator(names)
+#   badges = []
+#     names.each do |name|
+#       badges << "Hello, my name is #{name}."
+#     end
+#  badges
+
+# names.collect do |name|
+#     badge_maker(name)
+
+# end
+
+
+
+# def assign_rooms(names)
+
+#     rooms = []
+    
+#     names.each_with_index do |name, index|
+#         rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+# end
+
+# rooms
+# end
+
+# def printer(names)
+#     batch_badge_creator(names).each do |badge|
+#         puts badge
+#     end
+
+# end
+
