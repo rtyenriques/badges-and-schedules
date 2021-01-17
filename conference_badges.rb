d4b186e62cc70cfe44864c330cae4991281d56be
rtyenriques <tooshopboys@gmail.com>
2021-01-16 23:31:14 -0800

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def badge_maker(name)
   
   def assign_rooms(attendees)
     room=0
-    attendees.collect do |name|
+    attendees.each do |name|
       room+=1
       "Hello, #{name}! You'll be assigned to room #{room}!"
     end
